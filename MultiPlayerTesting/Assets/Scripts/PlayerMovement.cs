9dafed540f50dccb9fe6f11d34e1d0d0c1feae4e
tylerdeandigipen <71672056+tylerdeandigipen@users.noreply.github.com>
2022-03-22 13:02:53 -0700

    Update PlayerMovement.cs

--- MultiPlayerTesting/Assets/Scripts/PlayerMovement.cs
+++ MultiPlayerTesting/Assets/Scripts/PlayerMovement.cs
@@ -33,53 +33,20 @@ public class PlayerMovement : NetworkBehaviour
     }
     void FixedUpdate()
     {
-        if (IsServer)
-        {
-            UpdateServer();
-        }
         if (IsClient && IsOwner)
         {
             ClientInput();
-            UpdateClientPositionServerRpc(this.transform.position.x, this.transform.position.z);
         }
-    }
-    private void UpdateServer()
-    {
-        transform.position = new Vector3(xCord.Value, 0, zCord.Value);
     }
     private void ClientInput()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-        if (Input.GetKey(KeyCode.W))
-        {
-            vertical = 1;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            horizontal = -1;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            vertical = -1;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            horizontal = 1;
-        }
         if (vertical > 0 || vertical < 0)
             rb.AddForce(vertical > 0 ? Vector3.forward * walkSpeed : Vector3.back * walkSpeed);
         if (horizontal > 0 || horizontal < 0)
             rb.AddForce(horizontal > 0 ? Vector3.right * walkSpeed : Vector3.left * walkSpeed);
         zCord.Value = this.transform.position.z;
         xCord.Value = this.transform.position.x;
-    }
-
-
-    [ServerRpc]
-    public void UpdateClientPositionServerRpc(float xCord_, float zCord_)
-    {
-        zCord.Value = zCord_;
-        xCord.Value = xCord_;
     }
 }
